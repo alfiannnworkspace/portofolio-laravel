95b7654770de72277b8738cde63dccf6df1e0235
Muhammad Alfian <muhammad.alfianfird@gmail.com>
2026-05-17 10:25:22 +0700

    Fix: Mengalihkan storage Laravel ke /tmp untuk serverless Vercel

--- api/index.php
+++ api/index.php
@@ -1,2 +1,15 @@
 <?php
+// 1. Menyesuaikan arah server agar Laravel tidak bingung
+$_SERVER['SCRIPT_NAME'] = '/index.php';
+$_SERVER['SCRIPT_FILENAME'] = __DIR__ . '/../public/index.php';
+
+// 2. Mengalihkan semua proses tulis-menulis ke folder /tmp milik Vercel
+putenv('VIEW_COMPILED_PATH=/tmp');
+putenv('APP_CONFIG_CACHE=/tmp/config.php');
+putenv('APP_EVENTS_CACHE=/tmp/events.php');
+putenv('APP_PACKAGES_CACHE=/tmp/packages.php');
+putenv('APP_ROUTES_CACHE=/tmp/routes.php');
+putenv('APP_SERVICES_CACHE=/tmp/services.php');
+
+// 3. Memulai aplikasi Laravel
 require __DIR__ . '/../public/index.php';
